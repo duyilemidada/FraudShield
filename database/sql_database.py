24679b4fb91cca9df7209d833d2ad1c45492b88d
duyilemidada <duyilemidada51@gmail.com>
2026-05-21 10:21:24 +0100

    postgres modification

--- database/sql_database.py
+++ database/sql_database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm import DeclarativeBase
 from config import settings
-engine = create_engine(settings.DATABASE_URL, echo=False, connect_args={"check_same_thread": False})
+engine = create_engine(settings.DATABASE_URL, echo=False)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 class Base(DeclarativeBase):
